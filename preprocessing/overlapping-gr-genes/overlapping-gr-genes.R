f622cd17bd0a870222c3aa6bab5e5fb5b054d2df
mateuszzieba <mateuszzieba97@gmail.com>
2023-08-02 10:42:50 +0000

    Update analysis of overlapping genes
    optimize filtering algorithm, add new visualization

--- preprocessing/overlapping-gr-genes/overlapping-gr-genes.R
+++ preprocessing/overlapping-gr-genes/overlapping-gr-genes.R
@@ -1,56 +1,78 @@
-if (!require("BiocManager", quietly = TRUE))
+# List of CRAN packages
+cran_packages <- c("tidyverse", "magrittr", "circlize", "pheatmap")
+
+# List of Bioconductor packages
+bioc_packages <- c("ComplexHeatmap")
+
+# Function to install and load packages
+load_package <- function(pkg, is_bioc = FALSE) {
+  if (!requireNamespace(pkg, quietly = TRUE)) {
+    if (is_bioc) {
+      BiocManager::install(pkg)
+    } else {
+      install.packages(pkg)
+    }
+  }
+  library(pkg, character.only = TRUE)
+}
+
+# Check if BiocManager is installed, if not install
+if (!requireNamespace("BiocManager", quietly = TRUE)) {
   install.packages("BiocManager")
+}
 
-BiocManager::install("ComplexHeatmap")
+# Install and load CRAN packages
+lapply(cran_packages, load_package)
 
-install.packages("circlize")
-install.packages("pheatmap")
+# Install and load Bioconductor packages
+lapply(bioc_packages, load_package, is_bioc = TRUE)
 
-# load packages
-require(tidyverse)
-require(magrittr)
-require(ComplexHeatmap)
-require(circlize)
-library(pheatmap)
+# Check if biomaRt is installed
+if (!requireNamespace("biomaRt", quietly = TRUE)) {
+  install.packages("biomaRt")
+}
 
-source("preprocessing/overlapping-gr-genes/gr-gene-database-functions.  ")
-getwd()
-# read data
+detach(package:biomaRt)
+
+source("preprocessing/overlapping-gr-genes/gr-database-functions.R")
+source("preprocessing/overlapping-gr-genes/prepare-biomart-data.R")
+
+
+# Read data
 gr_gene_database_raw <- read.csv("data/overlapping-gr-genes/geneBase_060723.tsv", sep = "\t") 
 
-gr_gene_database_raw %>% head
-
-gr_gene_database_raw %>%  colnames()
-
+# Extract specific keys and values from the "Info" column
 gr_gene_database_raw %>% 
-  extract_keys_values("Info", c("tissue", "cell", "enviroment", " treatment", "dose", "time", "Log2Ratio")) -> gr_gene_database
+  extract_keys_values("Info", c("tissue", "cell", "enviroment", "treatment", "dose", "time", "Log2Ratio")) -> gr_gene_database
 
+# Preprocess the gene database
 gr_gene_database %>%   
-  # tail(100) %>% 
-  print_simple_database() %>% 
-  mutate(Source = str_replace_all(Source, "PMID: ", "")) %>% 
-  filter(Source != "marpiech_tissues_dex") %>% 
-  filter(Source != "michkor-cells-dex") %>% 
-  mutate(Gene_name = tolower(Gene_name)) %>% 
-  mutate(label = paste0(Source, "_", tissue, "_", cell)) -> gr_gene_database_preprocessing
+  print_simple_database() %>%
+  mutate(Source = str_replace_all(Source, "PMID: ", "")) %>%
+  filter(Source != "michkor-cells-dex") %>%
+  mutate(Gene_name = tolower(Gene_name)) %>%
+  mutate(label = paste0(Source, "_", tissue, "_", cell)) %>%
+  mutate(label = ifelse(Source == "marpiech_tissues_dex", paste0(Source, "_", Gene_list_number), label)) %>% 
+  filter(Gene_name %in% {filtered_biomart_by_go %>% .$gene_name %>% tolower() %>% unique()}) %>%
+  filter(label != "34362910_NA_NA") -> gr_gene_filter_biomart
 
 
-gr_gene_database_preprocessing %>% 
-  filter(Gene_name %in% {biomart_genes_in_go %>% .$gene_name %>% tolower() %>% unique()}) %>% 
-  filter(label != "34362910_NA_NA")-> gr_gene_filter_biomart
-  
-
+# Split gene names by label
 split(gr_gene_filter_biomart$Gene_name, gr_gene_filter_biomart$label) -> gr_gene_list
-total_genes = biomart_genes_in_go %>% .$gene_name %>% tolower() %>% unique()
 
-# Use the function
+# Get unique total genes
+total_genes = filtered_biomart_by_go %>% .$gene_name %>% tolower() %>% unique()
+
+# Perform chi-square tests
 results <- perform_chi2_tests(gr_gene_list, total_genes)
 
+# Create heatmaps
+pheatmap(log2(results$chi2_value_matrix+1), cluster_rows = T, cluster_cols = T, display_numbers = T)
+pheatmap(results$p_value_matrix, cluster_rows = T, cluster_cols = T, display_numbers = T)
 
-results$chi2_value_matrix -> chi2_value_matrix
-chi2_value_matrix <- ifelse(chi2_value_matrix > 5000, NA, chi2_value_matrix)
-chi2_value_matrix <- ifelse(chi2_value_matrix > 700, 700, chi2_value_matrix)
-
-
-pheatmap(chi2_value_matrix, cluster_rows = T, cluster_cols = T, display_numbers = T)
+Heatmap(log2(results$chi2_value_matrix+1), 
+        name = "overlap", 
+        col = color_palette, 
+        show_row_names = TRUE, 
+        show_column_names = TRUE)
 
--- preprocessing/overlapping-gr-genes/prepare-biomart-data.R
+++ preprocessing/overlapping-gr-genes/prepare-biomart-data.R
@@ -1,109 +1,85 @@
-if (!requireNamespace("BiocManager", quietly = TRUE)){
-  install.packages("BiocManager")
-}
-  
-BiocManager::install("biomaRt")
+#### Step 1: Obtain and Combine Gene Data from BioMart ####
+# Utilizing Ensembl Genes 110 for analysis
 
-require(readr)
-
-# require(biomaRt)
-
-#### Step 1: Obtain and combine gene data from BioMart ####
-
-# to analysis was used gene from: Ensembl Genes 110
-
-# The BioMart datasets for the three animals
+# Define BioMart datasets for three animals
 datasets <- list(
   human = "hsapiens_gene_ensembl",
   mouse = "mmusculus_gene_ensembl",
   rat = "rnorvegicus_gene_ensembl"
 )
 
-# Define a function to download and process the data from BioMart
+# Function to download and process data from BioMart
 download_biomart_data <- function(dataset, animal) {
-  # Connect to the Ensembl BioMart database
   mart <- biomaRt::useMart("ensembl")
-  # Select the dataset
   genes <- biomaRt::useDataset(dataset, mart)
-  # Query the data
   data <- biomaRt::getBM(attributes = c('external_gene_name', 'gene_biotype'), mart = genes)
-  # Rename and add animal column
   data <- data %>%
     rename(gene_name = external_gene_name) %>%
     mutate(animal = animal)
-  
   return(data)
 }
 
-# Apply the function to each dataset and bind the results together
+# Obtain BioMart genes and filter for protein coding genes
 biomart_genes <- purrr::map2_dfr(datasets, names(datasets), download_biomart_data)
+biomart_genes_protein <- biomart_genes %>% filter(gene_biotype == "protein_coding")
 
-# Filter the biomart_genes for protein coding genes
-biomart_genes_protein <- biomart_genes %>% 
-  filter(gene_biotype == "protein_coding")
-
-# check number of genes for each animal
-biomart_genes_protein %>% 
-  group_by(animal) %>% 
-  nest() %>% 
-  mutate(number_genes = map(data, ~nrow(.x))) %>% 
+# Check number of protein coding genes for each animal
+biomart_genes_protein %>%
+  group_by(animal) %>%
+  nest() %>%
+  mutate(number_genes = map(data, ~nrow(.x))) %>%
   unnest(number_genes)
 
-#### Step 2: Obtain and combine gene data from Gene Ontology ####
 
-# The URLs for the Gene Ontology data and corresponding animal names
+#### Step 2: Obtain and Combine Gene Data from Gene Ontology ####
+# Define URLs for Gene Ontology data
 urls <- c(
-  "http://geneontology.org/gene-associations/goa_human.gaf.gz",
+  "http://current.geneontology.org/annotations/goa_human.gaf.gz",
   "http://current.geneontology.org/annotations/mgi.gaf.gz",
   "http://current.geneontology.org/annotations/rgd.gaf.gz"
 )
-animals <- c("human", "mouse","rat")
+animals <- c("human", "mouse", "rat")
 
-# Define a function to download and process the Gene Ontology data
+# Function to download and process Gene Ontology data
 download_gene_ontology_data <- function(url, animal) {
-  # Read the data
-  data <- readr::read_tsv(url, skip = 1, comment = "!") 
-  # Select the third column (gene names), remove duplicates, and add animal column
+  data <- readr::read_tsv(url, skip = 1, comment = "!")
   data <- data %>%
     select(3) %>%
     unique() %>%
-    rename(gene_name = `...3`) %>%
+    set_colnames(c("gene_name")) %>%
     mutate(animal = animal)
-  
   return(data)
 }
 
-# Apply the function to each URL and bind the results together
+# Combine Gene Ontology data for each animal
 gene_ontology_genes <- purrr::map2_df(urls, animals, download_gene_ontology_data)
 
-# check number of genes for each animal
-gene_ontology_genes %>% 
-  group_by(animal) %>% 
-  nest() %>% 
-  mutate(number_genes = map(data, ~nrow(.x))) %>% 
+# Check number of genes for each animal in Gene Ontology data
+gene_ontology_genes %>%
+  group_by(animal) %>%
+  nest() %>%
+  mutate(number_genes = map(data, ~nrow(.x))) %>%
   unnest(number_genes)
 
 
-
-#### Step 3: Filter the BioMart genes based on the Gene Ontology data ####
-
-# Define a function to filter the BioMart data based on the Gene Ontology data
+#### Step 3: Filter BioMart Genes Based on Gene Ontology Data ####
+# Function to filter BioMart data based on Gene Ontology data
 filter_genes <- function(animal) {
-  # Select the rows for the current animal and check if gene_name is in the Gene Ontology data
   data <- biomart_genes_protein %>%
     filter(animal == !!animal) %>%
     filter(gene_name %in% gene_ontology_genes[gene_ontology_genes$animal == !!animal, ]$gene_name)
-  
   return(data)
 }
 
-# Apply the function to each animal and bind the results together
-biomart_genes_in_go <- purrr::map_dfr(animals, filter_genes)
+# Filter and combine results for each animal
+filtered_biomart_by_go <- purrr::map_dfr(animals, filter_genes)
 
-# check number of genes for each animal
-biomart_genes_in_go %>% 
-  group_by(animal) %>% 
-  nest() %>% 
-  mutate(number_genes = map(data, ~nrow(.x))) %>% 
+# Check number of filtered genes for each animal
+filtered_biomart_by_go %>%
+  group_by(animal) %>%
+  nest() %>%
+  mutate(number_genes = map(data, ~nrow(.x))) %>%
   unnest(number_genes)
 
+# Cleanup: Remove unnecessary variables
+rm(datasets, biomart_genes, biomart_genes_protein, urls, animals, gene_ontology_genes)
